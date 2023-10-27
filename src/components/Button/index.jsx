d75a3c7111a98db5c4c67b67f439a01cc7a7ac11
Victor Mateus <victormateus897@gmail.com>
2023-10-27 14:29:47 -0300

    Delete src directory

--- src/components/Button/index.jsx
+++
@@ -1,9 +0,0 @@
-import { Container } from "./styles";
-
-export function Button({ title, loading = false, $altercolors, ...rest }) {
-  return (
-    <Container disabled={loading} $altercolors={$altercolors} {...rest}>
-      {loading ? "Loading..." : title}
-    </Container>
-  );
-}
